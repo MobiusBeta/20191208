bc7d5fe87df35a840ba68b69df2134d205a7a37b
Mobius_Beta <405801769@qq.com>
2020-02-17 21:19:16 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("\n纪念所有在这些苦难中逝去的生命");
+/* JavaScript written by MaoRX.cn */
+var splashScreen="";
+
+if(splashScreen==="1"){
+	setTimeout(function(){
+		splashScr.style.display="none";
+	},5000);
+}else{
+	splashScr.style.display="none";
+}
+
+
