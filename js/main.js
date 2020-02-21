b899ee5d842a7d4c923e1fbe11163f79883f473c
Mobius_Beta <405801769@qq.com>
2020-02-21 20:57:36 +0800

    20w08c2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 console.log("\n纪念所有在这些苦难中逝去的生命");
 /* JavaScript written by R.X. Mao */
-var version="20w08c1";
+var version="20w08c2";
 var splashScreen="1";
 
+window.oncontextmenu=function(){return false;};
+window.onkeydown=function(e){if((e.ctrlKey||e.metaKey)&&e.keyCode==83)return false;};
 if(splashScreen==="1"){
 	setTimeout(function(){
 		splashScr.style.opacity="0";
